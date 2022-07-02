334c48a93f30dcd8655af17c0d67f9bc28ade761
antonio <tomgabinio@gmail.com>
2022-07-02 17:22:22 -0300

    .

--- exercicio_list_comprehension.py
+++ exercicio_list_comprehension.py
@@ -1,5 +1,10 @@
 
 string = '0123456789012345678901234567890123456789'
 n = 10
-comp = [(i, i + n) for i in range(0,len(string), n)]
-        #aqui eu estou fazendo uma tupla com 2 valores, o 
+lista = [string[i: i + n] for i in range(0,len(string), n)]
+
+retorno = '.'.join(lista)
+
+print(retorno)
+print(lista)
+
