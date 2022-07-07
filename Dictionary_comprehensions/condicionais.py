db6f1518f9b1e21d285cda6ebfc062a2c89018e3
antonio <tomgabinio@gmail.com>
2022-07-07 15:23:33 -0300

    main

--- Dictionary_comprehensions/condicionais.py
+++ Dictionary_comprehensions/condicionais.py
@@ -1,1 +1,17 @@
-numeros = [1, 2, 3, 4, 5]
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+novos_numeros = [numero for numero in numeros if numero > 5]
+# um filter nessa linha a qual pega somente os numeros maiores de 5
+
+impares = [numero for numero in numeros if numero % 2 != 0]
+# outro filter a qual ele pega somente os numeros impares
+
+pares = [numero for numero in numeros if numero % 2 == 0 or numero * 2 == 0]
+# outro filter so que com numeros pares
+
+outro_if = []
+
+print(numeros)
+print(novos_numeros)
+print(impares)
+print(pares)
