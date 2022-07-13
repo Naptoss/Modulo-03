7954648c9a7441d0d90f9ee6021544352a9916c5
antonio <tomgabinio@gmail.com>
2022-07-13 16:12:25 -0300

    alguma coisa

--- Zip and Zip_longest/Zip-e-Zip_longest.py
+++ Zip and Zip_longest/Zip-e-Zip_longest.py
@@ -13,10 +13,13 @@ cidades_estados = zip(estados, cidades)
 # Nesse zip ele ta pegando apenas os 3 respectivas cidades e estados
 # a saida disso sera a junção dos 3 iteraveis e deixando Monte Belo e o Outro sem
 # junção com os outros estados
+
 print(list(cidades_estados))
 
 
-# cidades_estados2 = zip_longest(cidades, estados)
+cidades_estados2 = zip_longest(cidades, estados)
 # No zip_longest aqui faz a mesma coisa que o zip, entretanto, quando chega
 # em Monte Belo e o Outro ele usa o None no [1] para repor a ausencia de uma
 # outro Estado
+
+print(list(cidades_estados2))
