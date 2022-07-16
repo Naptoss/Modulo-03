a024fed115ebad13de3892aa5f6d12d6b029c257
antonio <tomgabinio@gmail.com>
2022-07-16 14:52:29 -0300

    pra mim a melhor forma foi utilizando Dictionary

--- Exercicios/Somando.py
+++ Exercicios/Somando.py
@@ -1,5 +1,8 @@
 lista_a = [1, 2, 3, 4, 5, 6, 7]
 lista_b = [1, 2, 3, 4]
+lista_soma = [x + y for x, y in zip(lista_a, lista_b)]
+
+print(lista_soma)
 
 # lista_soma = []
 # for i in range(len(lista_b)):
@@ -8,7 +11,7 @@ lista_b = [1, 2, 3, 4]
 # print(lista_soma)
 
 
-lista_soma = []
-for i, _ in enumerate(lista_b):
-    lista_soma.append(lista_a[i] + lista_b[i])
-print(lista_soma)
+# lista_soma = []
+# for i, _ in enumerate(lista_b):
+#     lista_soma.append(lista_a[i] + lista_b[i])
+# print(lista_soma)
