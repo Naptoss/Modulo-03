cc195b3b4d45216c184dc3d41b69b1f5bf62460a
antonio <tomgabinio@gmail.com>
2022-07-16 14:48:22 -0300

    uma forma exclusiva que funciona somente em python

--- Exercicios/Somando.py
+++ Exercicios/Somando.py
@@ -1,8 +1,14 @@
 lista_a = [1, 2, 3, 4, 5, 6, 7]
 lista_b = [1, 2, 3, 4]
 
-lista_soma = []
-for i in range(len(lista_b)):
-    lista_soma.append(lista_a[i] + lista_b[i])
+# lista_soma = []
+# for i in range(len(lista_b)):
+#     lista_soma.append(lista_a[i] + lista_b[i])
 
+# print(lista_soma)
+
+
+lista_soma = []
+for i, _ in enumerate(lista_b):
+    lista_soma.append(lista_a[i] + lista_b[i])
 print(lista_soma)
