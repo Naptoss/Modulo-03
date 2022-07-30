ff9dfb1b418b5c1861ce5a67ca080d735fa6f267
antonio <tomgabinio@gmail.com>
2022-07-30 08:46:13 -0300

    Adicionado oque cada except faz

--- Anotações/main.py
+++ Anotações/main.py
@@ -3,10 +3,12 @@ try:
     print(a[1])
 except NameError as erro:
     print('Erro de Name', erro)
-    # T
-
+    # Ao retornar o erro de NameError, ele retorna o que esta no print
 
 except IndexError as erro:
     print('Error de Index ')
+    # Ao dar Erro de Index ele retorna no console a mensagem personalizada para
+    # o erro especifico.
+
 except Exception as erro:
     print('Error inesperado')
