7ce373eae05ab7fc70c453777ce87722cfc28fa2
antonio <tomgabinio@gmail.com>
2022-07-30 08:55:57 -0300

    Adicionado um tratamento para o KeyError juntamente ao IndexError

--- Anotações/main.py
+++ Anotações/main.py
@@ -5,8 +5,8 @@ except NameError as erro:
     print('Erro de Name', erro)
     # Ao retornar o erro de NameError, ele retorna o que esta no print
 
-except IndexError as erro:
-    print('Error de Index ')
+except (IndexError, KeyError) as erro:
+    print('Error de Index ou erro de chave ')
     # Ao dar Erro de Index ele retorna no console a mensagem personalizada para
     # o erro especifico.
 
